3082d7e91494da44af00533bf9fdcae2df3fb31e
Yen-Chung Chen <yenchung.chen@nyu.edu>
2021-02-18 07:37:20 -0500

    Formatting

--- wk2/backup.sh
+++ wk2/backup.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 # Defining the path of your project directory
-working_path="/scratch/ycc520/ag_examples/ag_recitation/wk2/"
+# working_path="/scratch/ycc520/ag_examples/ag_recitation/wk2/"
+working_path=$1
 
 # Make a time stamp so your backup files will be
 # named as something like 202102081125.tar.gz
 timestamp=$(date +"%Y%m%d_%H%M%S")
 filename="backup/$timestamp.tar.gz"
 
+echo $filename
+# backup/20210208_100818.tar.gz
 
 cd $working_path
 
